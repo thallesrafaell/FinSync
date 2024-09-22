cdfcb09955aae5044eeeb44809e5b1176d2fb6cc
Thalles <thallesrafaell@outlook.com>
2024-09-21 22:01:44 -0300

    feat: Initialcommit

---
+++ src/main/java/dev/thallesrafaell/FinSync/FinSyncApplication.java
@@ -0,0 +1,13 @@
+package dev.thallesrafaell.FinSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thallesrafaell/FinSync/FinSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thallesrafaell.FinSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
